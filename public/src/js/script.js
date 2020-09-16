a17aa89c0d18da5621d1fc5f2a1a25f547fae721
Marco <marco.teixeiradasilva@hotmail.fr>
2020-09-16 16:47:35 +0200

    todo: js search

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -3,6 +3,9 @@ const menuPopUp = document.querySelector('.menuPopUp');
 const menuPopOut = document.querySelector('.menuPopOut');
 const body = document.querySelector('.body')
 const podology = document.querySelector('.blueBackGround');
+const searchbloc = document.querySelector('.inputSearch');
+const searchlogo = document.querySelector('.searchLogo');
+
 
 btnBurger.addEventListener('click', function () {
     menuPopUp.classList.toggle('active');
@@ -15,3 +18,7 @@ menuPopOut.addEventListener('click', function() {
    menuPopOut.classList.toggle('active');
    body.classList.toggle('hidden');
 });
+
+searchlogo.addEventListener('click', function () {
+    searchbloc.classList.toggle('active');
+})
